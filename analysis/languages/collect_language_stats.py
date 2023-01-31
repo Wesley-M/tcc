dc134233036b793903197d61c0915f7c562dc93c
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2023-01-31 07:30:25 -0300

    Adding language stats

--- analysis/languages/collect_language_stats.py
+++ analysis/languages/collect_language_stats.py
@@ -1,8 +1,61 @@
-# Setting the github client
+import csv
+import json
+import os
+import re
 
+from dotenv import load_dotenv
+from github import Github
+
+# It collects language metadata from the repos
+
+languages_api = "https://api.github.com/repos/{}/{}/languages"
+languages_md = {}
+total_lines = 344
+
+# Github client
 load_dotenv()
 
 PERSONAL_TOKEN = os.getenv('PERSONAL_TOKEN')
 
 g = Github(PERSONAL_TOKEN)
 
+# Constants
+constants_f = open('../../utils/constants.json')
+constants = json.load(constants_f)
+
+HEADER = constants["AWESOME_HEADER_ENUM"]
+
+def languages(link):
+    """ Returns the languages for a project """
+    fullname = re.search("https://github.com/(.*)$", link).group(1).rstrip("/")
+    try:
+        return g.get_repo(fullname).get_languages()
+    except:
+        print("Failed on: {}".format(fullname))
+
+with open(
+    "../../scraping/repo_list/data/awesome_ml_mv.csv",
+    "r",
+    encoding="utf-8",
+    newline="",
+) as f:
+    reader = csv.reader(f, dialect="unix")
+
+    # headers: ['category', 'name', 'link', 'description']
+    next(reader)
+        
+    i = 0
+    for row in reader:
+        owner, repo = re.search("https://github.com/(.*)$", row[HEADER["LINK"]]).group(1).rstrip("/").split('/')
+        
+        print('---')
+        print(f'[{round((i+1) * 100 / total_lines, 1)}%] Requesting languages from {owner}/{repo}:')
+        
+        languages_md[row[HEADER["NAME"]]] = languages(row[HEADER["LINK"]])
+
+        print(languages_md)
+    
+        i += 1
+
+with open('data/languages_metadata.json', 'w', encoding='utf-8') as outfile:
+    json.dump(languages_md, outfile)
---
+++ analysis/languages/python_stats.py
@@ -0,0 +1,34 @@
+import json
+
+"""
+It reads from languages_metadata and extracts python stats
+"""
+
+metadata_file = open('data/languages_metadata.json')
+
+metadata = json.load(metadata_file)
+
+with open('data/python_metadata.csv', 'w') as f:
+    f.write("repo,py_percentage\n")
+    i = 0
+    for repo in metadata.keys():
+        total = 0
+        python = 0
+
+        for language in metadata[repo]:
+            bytes = metadata[repo][language]
+            total += bytes
+
+            if language == 'Python':
+                python = bytes
+
+        if total == 0 and python == 0:
+            python = 1
+            total = 1
+        
+        print(f"[{i}]: {repo} | python: {python}, total: {total}, prop: {round(python * 100 / total, 2)}")
+        i += 1
+
+        f.write("%s,%s\n" % (repo, round(python * 100 / total, 2)))
+
+metadata_file.close()
