fef2b6d5f7615cad9bd43f1b99d45d1452377d66
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2023-02-05 21:46:42 -0300

    Updating README.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-1. Generate a github token to use the Github API. After that, create a .env file with yout token as follows:
-    - PERSONAL_TOKEN=YOUR_TOKEN
+## Understanding the testing culture of machine learning projects on Github
+
+The current repository serves as a storage for all scripts used for the paper purposes.
+
+Before we dive in deeper in the repo structure, make sure to have a Github API token setup.
+Several od the scripts depend on it.
+
+    Create a .env file with your token on the root dir, with the content:
+        PERSONAL_TOKEN=YOUR_TOKEN
+
+### Where to start ?
+
+First, you should run the "setup.py" script. It will clone all the machine learning
+repos on your machine for further analysis.
+
+### Repository folder structure
+
+The repository is structured as follows:
+
+1. The scraping folder is composed of repo_list and tests. These contain the scripts
+for scraping the "Awesome machine learning repository" and all the test statistics. Each
+folder has a README.md, where you can find the instructions and usage of the scripts;
+
+2. The plots folder has all R scripts being used to plot the paper tables and figures;
+
+3. Analysis folder is composed of coverage, issues, languages and stats sub-folders. Each
+one of them have their respective scripts and instructions. 
--- plots/show_coverage.R
+++ plots/show_coverage.R
@@ -32,12 +32,52 @@ merged <- merged %>%
         )
     ))
 
+print(nrow(github_metadata))
+
 merged %>%
     select(tracking_tool) %>%
     group_by(tracking_tool) %>%
     rename("Coverage tracking tool" = tracking_tool) %>%
-    summarise("# of projects that adopt the tool" = n()) %>%
+    summarise("# of projects that adopt the tool" = n(), "%" = paste(round(n() / nrow(github_metadata) * 100, 2), "%")) %>%
     arrange(desc(`# of projects that adopt the tool`)) %>%
     kbl() %>%
     kable_styling(full_width = F) %>%
     save_kable(here(add_base_dir("plots/images/coverage_tracking_tool.png")))
+
+total_projects <- github_metadata %>%
+    group_by(type) %>%
+    summarise(tot = n())
+
+old_cov_by_type <- merged %>%
+        group_by(type) %>%
+        summarise(n = n(), mean = mean(coverage)) %>%
+        arrange(desc(mean))
+
+cov_by_type <- merge(total_projects, old_cov_by_type, by = "type") %>%
+    arrange(desc(mean)) %>%
+    mutate(
+        proportion_abs = paste0(n, "/", tot),
+        proportion = paste(round(n / tot * 100, 2), "%"), 
+        mean = paste(round(mean, 2), "%")
+    ) %>%
+    select(type, proportion_abs, proportion, mean) %>%
+    rename(
+        "# of projects that publish coverage" = proportion_abs,
+        "%" = proportion,
+        "Mean coverage (%)" = mean,
+        "Category" = type
+    )
+
+totals <- merge(total_projects, old_cov_by_type, by = "type") %>%
+    ungroup() %>%
+    summarise(
+        "# of projects that publish coverage" = paste0(nrow(cov), "/", nrow(github_metadata)),
+        "%" = paste(round(nrow(cov) / nrow(github_metadata) * 100, 2), "%"),
+        "Mean coverage (%)" = paste(round(mean(cov$coverage), 2), "%"),
+        "Category" = "Overall"
+    )
+
+rbind(cov_by_type, totals) %>%
+    kbl() %>%
+    kable_styling(full_width = F) %>%
+    save_kable(here(add_base_dir("plots/images/cov_by_type.png")))
